0ba23e9499f722937a670df30bfa34bed0c8af0a
giofeder <giofeder@99afaf0d-6903-0410-885a-c66a8bbb5f81>
2015-01-16 11:17:40 +0000

    git-svn-id: svn://svn.forge.objectweb.org/svnroot/spagobi/V_4.x/Server/trunk@24423 99afaf0d-6903-0410-885a-c66a8bbb5f81

--- SpagoBIConsoleEngine/src/it/eng/spagobi/engines/console/exporter/types/utils/CSVDocument.java
+++ SpagoBIConsoleEngine/src/it/eng/spagobi/engines/console/exporter/types/utils/CSVDocument.java
@@ -1,42 +1,41 @@
 /* SpagoBI, the Open Source Business Intelligence suite
 
  * Copyright (C) 2012 Engineering Ingegneria Informatica S.p.A. - SpagoBI Competency Center
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice. 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0, without the "Incompatible With Secondary Licenses" notice.
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package it.eng.spagobi.engines.console.exporter.types.utils;
 
-
 import java.util.Vector;
 
-public class CSVDocument{
+public class CSVDocument {
 
 	private Vector<String> header;
 	private Vector<Row> rows;
-	
+
 	Character separator;
 
-	
-	
-	
-	public class Row{
+	public class Row {
 		private Vector<String> columns;
+
 		public Row() {
 			columns = new Vector<String>();
 		}
+
 		public Vector<String> getColumns() {
 			return columns;
 		}
+
 		public void setColumns(Vector<String> columns) {
 			this.columns = columns;
 		}
-		
+
 	}
 
 	public CSVDocument() {
 		super();
 		header = new Vector<String>();
 		rows = new Vector<Row>();
-		separator = ',';
+		separator = ';';
 	}
 
 	public Vector<String> getHeader() {
@@ -62,10 +61,5 @@ public class CSVDocument{
 	public void setSeparator(Character separator) {
 		this.separator = separator;
 	}
-
-
-
-
-
 
 }
